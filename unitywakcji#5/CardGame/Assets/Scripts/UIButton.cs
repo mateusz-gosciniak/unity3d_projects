edcbe31e1b784b0f29b76458f32328eb1ac1ac08
Mateusz Gościniak <gosciniak.mateusz@outlook.com>
2019-11-24 19:27:10 +0100

    unitywakcji$5

---
+++ unitywakcji#5/CardGame/Assets/Scripts/UIButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject = null;
+    [SerializeField] private string targetMessage = null;
+    public Color highlightColor = Color.cyan;
+    
+    private void OnMouseEnter() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null) {
+            sprite.color = highlightColor;
+        }
+    }
+    private void OnMouseExit() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null) {
+            sprite.color = Color.white;
+        }
+    }
+    void OnMouseDown() {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+    private void OnMouseUp() {
+        transform.localScale = Vector3.one;
+        if (targetObject != null) {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
