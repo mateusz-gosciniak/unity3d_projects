cc652d6820499047a31408f43092099de3868f12
Mateusz Gościniak <gosciniak.mateusz@outlook.com>
2019-11-24 19:34:40 +0100

    test

---
+++ Game2DJoyTouch/Assets/Scenes/InputController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private CharacterController _player = null;
+    public float speed = 10.0f;
+    public float gravity = -9.8f;
+
+    void Start(){
+        _player = this.gameObject.GetComponent<CharacterController>();
+    }
+    void Update() {
+        if (_player == null) return;
+
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaY = gravity + Input.GetAxis("Vertical") * speed;
+        Vector3 translateVector = new Vector3(deltaX, deltaY, 0.0f);
+        translateVector = Vector3.ClampMagnitude(translateVector, speed);
+        translateVector *= Time.deltaTime;
+        translateVector = transform.TransformDirection(translateVector);
+        _player.Move(translateVector);
+        
+        if (Input.GetKey(KeyCode.Escape)){
+            Application.Quit();
+        }
+    }
+}
---
+++ test/Assets/Scenes/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private CharacterController _playerControl = null;
+    public float speed = 3.0f;
+    public float gravity = -9.8f;
+
+    private void Start() {
+        _playerControl = this.gameObject.GetComponent<CharacterController>();
+    }
+    void Update()
+    {    
+        if (_playerControl == null) return;
+
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0.0f, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        _playerControl.Move(movement);
+    }
+}
